c49e30fc6f9912ab143882b9f2d9c552b4411750
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-02-05 15:19:31 +0500

    AddGameStateMashin

---
+++ CoopProject/Assets/Scripts/Infrostracture/GameStateMashin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameStateMashin
+{
+    private readonly Dictionary<Type, IState> _states;
+    private IState _activeState;
+
+    public GameStateMashin()
+    {
+        _states = new Dictionary<Type, IState>();
+    }
+    
+    
+    public void Enter<TState>() where TState : IState
+    {
+        _activeState?.Exit();
+        IState state = _states[typeof(TState)];
+        state.Enter();
+    }
+}
