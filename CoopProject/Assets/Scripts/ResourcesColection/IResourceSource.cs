60795bef5469590e433957676e7155e9ff3a2a0a
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-03-05 21:02:44 +0500

    Factory-helper

---
+++ CoopProject/Assets/Scripts/ResourcesColection/IResourceSource.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ResourcesColection
+{
+    public interface ResourceSource
+    {
+         void TakeDamage(int damage);
+    }
+}
--- CoopProject/Assets/Scripts/ResourcesColection/ResourceSource.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ResourcesColection
-{
-    public abstract class ResourceSource : MonoBehaviour
-    {
-        public abstract void TakeDamage(int damage);
-    }
-}
