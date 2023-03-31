05f395a4ab4eecad5f7fa5ea7b703335fc62fca7
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-04-01 00:33:54 +0500

    AddNewSpawner

--- CoopProject/Assets/Scripts/ResourcesColection/Rock/Rock.cs
+++ CoopProject/Assets/Scripts/ResourcesColection/Rock/Rock.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using Reflex;
 using Reflex.Scripts.Attributes;
 using ResourcesColection;
+using ResourcesGame.TypeResource;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(BoxCollider))]
 public class Rock : ResourceSource,IResourceSource
 {
-    private int _resourceValue = 15;
     private int _maxHealth = 10;
     private int _health = 30;
     private float _durationReset = 1f;
+    private ResourceCollector _resourceCollector;
 
-
-
+    [Inject]
+    private void Inject(Container container) => _resourceCollector = container.Resolve<ResourceCollector>();
+    
+    private void Awake()
+    {
+        _mesh = GetComponent<MeshRenderer>();
+        _colliderBox = GetComponent<BoxCollider>();
+    }
+    
     public override void TakeDamage(int damage)
     {
         _health -= damage;
@@ -21,8 +31,11 @@ public class Rock : ResourceSource,IResourceSource
         {
             Dead();
             _iDead = true;
+            Stone stone = new ();
+            stone.SetPrice();
+            _resourceCollector.AddResource<Stone>(stone);
+            StartCoroutine(Reset());
         }
-        
     }
 
     private IEnumerator Reset()
