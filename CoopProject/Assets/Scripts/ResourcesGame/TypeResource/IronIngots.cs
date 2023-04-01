3351d087a803232d2e75f498301a87f69f88c99b
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-04-01 15:53:06 +0500

    AddPanelIron

--- CoopProject/Assets/Scripts/ResourcesGame/TypeResource/IronIngots.cs
+++ CoopProject/Assets/Scripts/ResourcesGame/TypeResource/IronIngots.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 namespace ResourcesGame.TypeResource
 {
-    public class IronIngots : MonoBehaviour
+    public class IronIngots : Resource
     {
-        
+        private int priceLog = 65;
+        public void SetPrice()=> Price = priceLog;
     }
 }
--- CoopProject/Assets/Scripts/UI/ProductPanel.cs
+++ CoopProject/Assets/Scripts/UI/ProductPanel.cs
@@ -1,92 +1,85 @@
-using Reflex;
-using Reflex.Scripts.Attributes;
 using ResourcesGame.TypeResource;
 using TMPro;
 using UnityEngine;
 
-public abstract class ProductPanel<T> : MonoBehaviour
+public abstract class ProductPanel : MonoBehaviour
 {
-   [SerializeField] private TextMeshProUGUI _textCount;
-   [SerializeField] private TextMeshProUGUI _textEndCount;
-   [SerializeField] protected T _resourceType;
-   [SerializeField] private int _stack = 10;
-   
-   private ResourceCollector _resourceCollector;
-   private int _counter = 0;
-   private int _addResourceCount = 0;
-   
-   [Inject]
-   private void Inject(Container container) => _resourceCollector = container.Resolve<ResourceCollector>();
+    [SerializeField] private TextMeshProUGUI _textCount;
+    [SerializeField] private TextMeshProUGUI _textEndCount;
+    [SerializeField] private int _stack = 10;
 
-   private void Start()
-   {
-      _textCount.text = $"{_counter}";
-      _textEndCount.text = $"{_addResourceCount}";
-   }
+    protected ResourceCollector _resourceCollector;
+    private int _counter = 0;
+    private int _addResourceCount = 0;
 
-   public void SetCount()
-   {
-      if (ValiadateAdd())
-      {
-         _counter++;
-         _addResourceCount = _counter;
-         _textCount.text = $"{_counter}";
-         _textEndCount.text = $"{_addResourceCount}";
-      }
-   }
+    private void Start()
+    {
+        _textCount.text = $"{_counter}";
+        _textEndCount.text = $"{_addResourceCount}";
+    }
 
-   public void SetStackCount()
-   {
-      int countList = _resourceCollector.GetCountList<Log>();
-      
-      if (countList > _stack && _counter + _stack <= countList)
-      {
-         _counter += _stack;
-         _addResourceCount = _counter;
-         _textCount.text = $"{_counter}";
-         _textEndCount.text = $"{_addResourceCount}";
-      }
-   }
+    protected void SetCount<T>()
+    {
+        if (ValiadateAdd<T>())
+        {
+            _counter++;
+            _addResourceCount = _counter;
+            _textCount.text = $"{_counter}";
+            _textEndCount.text = $"{_addResourceCount}";
+        }
+    }
 
-   public void Take()
-   {
-      if (_counter + _stack <= 0)
-      {
-         _counter -= _stack;
-         _textCount.text = $"{_counter}";
-         _addResourceCount = _counter;
-         _textEndCount.text = $"{_addResourceCount}";
-      }
-   }
+    protected void SetStackCount<T>()
+    {
+        int countList = _resourceCollector.GetCountList<T>();
 
-   public void TakeResource()
-   {
-      for (int i = 0; i < _addResourceCount;i++)
-      {
-         var boards = new Boards();
-         boards.SetPrice();
-         _resourceCollector.AddResource<Boards>(boards);
-      }
-      Reset();
-   }
+        if (countList > _stack && _counter + _stack <= countList)
+        {
+            _counter += _stack;
+            _addResourceCount = _counter;
+            _textCount.text = $"{_counter}";
+            _textEndCount.text = $"{_addResourceCount}";
+        }
+    }
 
-   private bool ValiadateAdd()
-   {
-      int countList = _resourceCollector.GetCountList<Log>();
-      
-      if (_counter < countList)
-         return true;
+    protected void Take()
+    {
+        if (_counter + _stack <= 0)
+        {
+            _counter -= _stack;
+            _textCount.text = $"{_counter}";
+            _addResourceCount = _counter;
+            _textEndCount.text = $"{_addResourceCount}";
+        }
+    }
 
-      return false;
-   }
+    protected void TakeResource<Type>(Resource resource)
+    {
+        for (int i = 0; i < _addResourceCount; i++)
+            _resourceCollector.AddResource<Type>(resource);
+        
+    }
+
+    private bool ValiadateAdd<T>()
+    {
+        int countList = _resourceCollector.GetCountList<T>();
+
+        if (_counter < countList)
+            return true;
+
+        return false;
+    }
 
 
-   private void Reset()
-   {
-      _counter = 0;
-      _addResourceCount = 0;
-      _textCount.text = $"{_counter}";
-      _textEndCount.text = $"{_addResourceCount}";
-      _resourceCollector.SallResource<Log>();
-   }
+   protected void Reset<Type>()
+    {
+        if (_counter > 0 && _addResourceCount > 0)
+        {
+            _counter = 0;
+            _addResourceCount = 0;
+            _textCount.text = $"{_counter}";
+            _textEndCount.text = $"{_addResourceCount}";
+            _resourceCollector.SallResource<Type>();
+        }
+    }
 }
