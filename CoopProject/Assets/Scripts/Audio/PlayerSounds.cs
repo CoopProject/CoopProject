be17feb4f37cb301ef735dfd9ab6ef2013a66553
Daniil Gorin <daniil009.gorin@yandex.ru>
2023-04-08 10:23:51 +0300

    Update player sound script

--- CoopProject/Assets/Scripts/Audio/PlayerSounds.cs
+++ CoopProject/Assets/Scripts/Audio/PlayerSounds.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStepsSounds : MonoBehaviour
+public class PlayerSounds : MonoBehaviour
 {
     [SerializeField] private List<AudioClip> _steps;
     [SerializeField] private AudioSource _stepsPlayer;
+    [SerializeField] private AudioSource _miningSound;
     
-    public void Play()
+    public void PlayStepSound()
     {
         int index = Random.Range(0, _steps.Count);
         _stepsPlayer.PlayOneShot(_steps[index]);
+    }
+
+    public void PlayMiningSound()
+    {
+        _miningSound.Play();
     }
 }
