b065e0cf7d0b638e7d92bc3c6eb8f491308b31d2
Daniil Gorin <daniil009.gorin@yandex.ru>
2023-05-02 07:55:31 +0300

    Correction load screen script

--- CoopProject/Assets/LoadingSceen.cs
+++ CoopProject/Assets/LoadingSceen.cs
@@ -8,13 +8,10 @@ public class LoadingSceen : MonoBehaviour
     [SerializeField] private AudioSource _backgroundSound;
     [SerializeField] private RectTransform _transformIcon;
     [SerializeField] private Animator _animator;
-    [SerializeField] private GameObject _joustic;
     [SerializeField] private GameObject _water;
     [SerializeField] private StartLearningControl _startLerningControl;
  
-    private float _valueFade = 0;
     private float _durationStartGame = 30f;
-    private float _spead = 1f;
 
     private void Awake()
     {
@@ -27,22 +24,22 @@ public class LoadingSceen : MonoBehaviour
         _animator.GetComponent<Animator>();
         _animator.Play("Loading");
         StartCoroutine(StartGame());
-        _joustic.gameObject.SetActive(false);
         _water.gameObject.SetActive(false);
     }
 
     private IEnumerator StartGame()
     {
         var WaitForSecondsRealtime = new WaitForSecondsRealtime(1f);
+
         while (_durationStartGame >= 0)
         {
             _durationStartGame -= 1f;
             yield return WaitForSecondsRealtime;
         }
+
         _backgroundSound.Play();
-        _joustic.gameObject.SetActive(true);
         _water.gameObject.SetActive(true);
         _startLerningControl.gameObject.SetActive(true);
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
 }
